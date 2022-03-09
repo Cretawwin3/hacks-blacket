376b16f78bbaf4d7900ac4b32529bbeb56b7c6b2
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-03-09 11:43:43 -0600

    Update fixPage.js

--- global/fixPage.js
+++ global/fixPage.js
@@ -1,3 +1,12 @@
-fetch("https://raw.githubusercontent.com/ZasticBradyn/blacket-hacks/main/resources/fixPage.js")
-.then((res) => res.text()
-.then((t) => eval(t)))
+fetch('https://raw.githubusercontent.com/ZasticBradyn/blacket-hacks/main/resources/fixPage.js').then(function(response) {
+    if (!response.ok) {
+        cosole.error("error fetching resource")
+    }
+    return response.blob();
+}).then(function(myBlob) {
+    var objectURL = URL.createObjectURL(myBlob);
+    var sc = document.createElement("script");
+    sc.setAttribute("src", objectURL);
+    sc.setAttribute("type", "text/javascript");
+    document.head.appendChild(sc);
+})
