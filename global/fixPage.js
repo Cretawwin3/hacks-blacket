e141fb04400f23bf490c62c916dff418a6fd96fd
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-03-09 11:21:18 -0600

    Update fixPage.js

--- global/fixPage.js
+++ global/fixPage.js
@@ -1,1 +1,70 @@
-//code soon
+site = window.location.pathname;
+
+fetch('https://raw.githubusercontent.com/ZasticBradyn/blacket-hacks/main/resources/error.js').then(function(response) { 
+        if (!response.ok) { 
+            cosole.log("error fetching error.js")
+        } 
+        return response.blob(); 
+    }) .then(function(myBlob) { 
+        var objectURL = URL.createObjectURL(myBlob); 
+        var sc = document.createElement("script");
+        sc.setAttribute("src", objectURL); 
+        sc.setAttribute("type", "text/javascript"); 
+        document.head.appendChild(sc);
+    })
+
+switch (site) {
+    case "/market.php":
+        fetch('https://raw.githubusercontent.com/ZasticBradyn/blacket-hacks/main/resources/market.js').then(function(response) {
+            if (!response.ok) {
+                cosole.log("error fetching modified code")
+            }
+            return response.blob();
+        }).then(function(myBlob) {
+            var objectURL = URL.createObjectURL(myBlob);
+            var sc = document.createElement("script");
+            sc.setAttribute("src", objectURL);
+            sc.setAttribute("type", "text/javascript");
+            document.head.appendChild(sc);
+        })
+  case "blooks.php":
+    fetch('https://raw.githubusercontent.com/ZasticBradyn/blacket-hacks/main/resources/blooks.js').then(function(response) {
+            if (!response.ok) {
+                cosole.log("error fetching modified code")
+            }
+            return response.blob();
+        }).then(function(myBlob) {
+            var objectURL = URL.createObjectURL(myBlob);
+            var sc = document.createElement("script");
+            sc.setAttribute("src", objectURL);
+            sc.setAttribute("type", "text/javascript");
+            document.head.appendChild(sc);
+        })
+  case "admin_boxes.php":
+    fetch('https://raw.githubusercontent.com/ZasticBradyn/blacket-hacks/main/resources/adminbox.js').then(function(response) {
+            if (!response.ok) {
+                cosole.log("error fetching modified code")
+            }
+            return response.blob();
+        }).then(function(myBlob) {
+            var objectURL = URL.createObjectURL(myBlob);
+            var sc = document.createElement("script");
+            sc.setAttribute("src", objectURL);
+            sc.setAttribute("type", "text/javascript");
+            document.head.appendChild(sc);
+        })
+  case "admin_blooks.php":
+    fetch('https://raw.githubusercontent.com/ZasticBradyn/blacket-hacks/main/resources/adminblook.js'.then(function(response) {
+            if (!response.ok) {
+                cosole.log("error fetching modified code")
+            }
+            return response.blob();
+        }).then(function(myBlob) {
+            var objectURL = URL.createObjectURL(myBlob);
+            var sc = document.createElement("script");
+            sc.setAttribute("src", objectURL);
+            sc.setAttribute("type", "text/javascript");
+            document.head.appendChild(sc);
+        })
+
+}
