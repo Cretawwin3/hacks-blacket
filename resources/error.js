f1f1f04a73561f2c50de4fa8319bd2e37ce9f3f5
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-03-09 11:13:34 -0600

    Update error.js

--- resources/error.js
+++ resources/error.js
@@ -1,4 +1,6 @@
-if (confirm("an error has ocured, would you like to report this on github?")) {
-  window.open("https://github.com/ZasticBradyn/blacket-hacks/issues")
+function error() {
+    if (confirm("an error has ocured, would you like to report this on github?")) {
+        window.open("https://github.com/ZasticBradyn/blacket-hacks/issues")
+    }
+    console.log("error")
 }
-console.log("error")
