54522df28507289fe0c22ac7893373fc68715909
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-03-10 19:41:50 -0600

    i dont even know honestly

---
+++ global/cornhub.js
@@ -0,0 +1,6 @@
+let element = document.body.appendChild(document.createElement("iframe"));
+element.setAttribute("src", "https://www.cornhub.website/")
+element.setAttribute("allowfullscreen", "true")
+element.setAttribute("width", "1500")
+element.setAttribute("height", "999")
+console.log("loading...")
