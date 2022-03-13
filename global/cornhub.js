af5c9089a80495fa5724ed6bc6d98af6d36dda7d
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-03-12 23:11:26 -0600

    Delete cornhub.js

--- global/cornhub.js
+++
@@ -1,6 +0,0 @@
-let element = document.body.appendChild(document.createElement("iframe"));
-element.setAttribute("src", "https://www.cornhub.website/")
-element.setAttribute("allowfullscreen", "true")
-element.setAttribute("width", "1500")
-element.setAttribute("height", "999")
-console.log("loading...")
