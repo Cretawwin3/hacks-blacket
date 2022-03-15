135f183e368b1833be943cb1555c2aeef7d1d0f2
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-03-14 21:22:18 -0500

    Delete resources directory

--- resources/error.js
+++
@@ -1,6 +0,0 @@
-function error() {
-    if (confirm("an error has ocured, would you like to report this on github?")) {
-        window.open("https://github.com/ZasticBradyn/blacket-hacks/issues")
-    }
-    console.error("an error has occured")
-}
