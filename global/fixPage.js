c1575a10073286befb800e29453359981d0fe388
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-03-15 21:42:59 -0500

    Update fixPage.js

--- global/fixPage.js
+++ global/fixPage.js
@@ -1,18 +1,5 @@
 site = window.location.pathname;
 
-fetch('https://raw.githubusercontent.com/ZasticBradyn/blacket-hacks/main/resources/error.js').then(function(response) {
-    if (!response.ok) {
-        cosole.log("error fetching error.js")
-    }
-    return response.blob();
-}).then(function(myBlob) {
-    var objectURL = URL.createObjectURL(myBlob);
-    var sc = document.createElement("script");
-    sc.setAttribute("src", objectURL);
-    sc.setAttribute("type", "text/javascript");
-    document.head.appendChild(sc);
-})
-
 switch (site) {
     case "/market.php":
         $.get('/worker/misc/getmaxid.php', function(data) {
@@ -27,18 +14,19 @@ switch (site) {
         setTimeout(() => showBoxes(), 250);
         break;
     case "blooks.php":
-        fetch('https://raw.githubusercontent.com/ZasticBradyn/blacket-hacks/main/resources/blooks.js').then(function(response) {
-            if (!response.ok) {
-                cosole.log("error fetching modified code")
+        $.get('/worker/misc/getmaxblooks.php', function(data) {
+            window.maxID = data
+            for (let i = 1; i <= maxID; i++) {
+                window.gotBlooks = true
+                $('<code>', {
+                    id: `blook${i}`
+                }).appendTo('#blookAdder');
             }
-            return response.blob();
-        }).then(function(myBlob) {
-            var objectURL = URL.createObjectURL(myBlob);
-            var sc = document.createElement("script");
-            sc.setAttribute("src", objectURL);
-            sc.setAttribute("type", "text/javascript");
-            document.head.appendChild(sc);
-        })
+        });
+        $.get('/worker/blook/getrandomblook.php', function(data) {
+            setBlookInfo(data);
+        });
+        setTimeout(() => showBlooks(), 250);
         break;
     case "admin_boxes.php":
         fetch('https://raw.githubusercontent.com/ZasticBradyn/blacket-hacks/main/resources/adminbox.js').then(function(response) {
