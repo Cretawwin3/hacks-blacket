2b89d8b8e5e67bc8f2f6a8456b7433af39480a76
Cretawwin3 <97013168+Cretawwin3@users.noreply.github.com>
2022-04-03 10:36:04 +0700

    spam open boxes fix updace

--- global/SpamOpenBoxes.js
+++ global/SpamOpenBoxes.js
@@ -10,9 +10,26 @@ if (confirm("Would you like to select the amount of boxes?\nOk - Yes\nCancel - N
 function buyBox() {
     var postData = 'box=' + name;
     $.post('/worker/box/openbox.php', postData, function(data) {
-        var myArray = data.split("|");
-        var word = myArray[0];
-        console.log(word);
+        dataSplit = data.split('|')
+        window.blookRarity = dataSplit[1];
+        window.blookUnlocked = dataSplit[0];
+        if (blookRarity === "Uncommon") {
+            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px lime;', `${blookUnlocked}`);
+        } else if (blookRarity === "Rare") {
+            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px blue;', `${blookUnlocked}`);
+        } else if (blookRarity === "Epic") {
+            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px red;', `${blookUnlocked}`);
+        } else if (blookRarity === "Legendary") {
+            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px gold;', `${blookUnlocked}`);
+        } else if (blookRarity === "Chroma") {
+            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px skyblue;', `${blookUnlocked}`);
+        } else if (blookRarity === "Perfect") {
+            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px lemonchiffon;', `${blookUnlocked}`);
+        } else if (blookRarity === "Mystical") {
+            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px plum;', `${blookUnlocked}`);
+        } else if (blookRarity === "Divine") {
+            console.log('%c%s', 'color: white; font-size: 25px; text-shadow: 0px 0px 15px violet;', `${blookUnlocked}`);
+        } else if (blookRarity === undefined) {}
         if (data === "You're being rate limited.") {
             i--;
         }
